5b1fef4b503988129ff4ec0c00d4b5a714476a0e
mhihmi <mehdihilmi@gmail.com>
2025-04-25 21:45:40 +0200

    add variables

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key pair name for SSH access"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "The name tag for the EC2 instance"
+  type        = string
+}
--- variables.tf
+++ variables.tf
@@ -22,3 +22,24 @@ variable "sso_role_arn" {
   description = "The ARN of the SSO role allowed to access the S3 bucket"
   type        = string
 }
+
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key pair name for SSH access"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "The name tag for the EC2 instance"
+  type        = string
+}
