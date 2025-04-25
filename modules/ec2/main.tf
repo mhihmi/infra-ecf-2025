8424191c5167cf2e5b616bb52c8139d752b3cc53
mhihmi <mehdihilmi@gmail.com>
2025-04-25 21:46:25 +0200

    EC2 setup with tomcat

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,6 +3,28 @@ resource "aws_instance" "this" {
   instance_type = var.instance_type
   key_name      = var.key_name
 
+  user_data = <<-EOF
+    #!/bin/bash
+    # Package update
+    sudo yum update -y
+
+    # Install Java (Required for Tomcat)
+    sudo yum install -y java-1.8.0-openjdk
+
+    # Download and install Tomcat Tomcat
+    cd /opt
+    sudo curl -O https://downloads.apache.org/tomcat/tomcat-9/v9.0.73/bin/apache-tomcat-9.0.73.tar.gz
+    sudo tar -xvzf apache-tomcat-9.0.73.tar.gz
+    sudo mv apache-tomcat-9.0.73 tomcat
+
+    # Start Tomcat
+    sudo chmod +x /opt/tomcat/bin/*.sh
+    sudo /opt/tomcat/bin/startup.sh
+
+    # Setup Tomcat to start on reboot
+    echo "@reboot root /opt/tomcat/bin/startup.sh" | sudo tee -a /etc/crontab
+  EOF
+
   tags = {
     Name = var.instance_name
   }
