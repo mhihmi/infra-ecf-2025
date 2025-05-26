848fcf40a88656e8d077eca40ddf817db70a719d
mhihmi <mehdihilmi@gmail.com>
2025-05-26 18:48:23 +0200

    add secret manager + terraform fmt to indent

--- main.tf
+++ main.tf
@@ -13,6 +13,24 @@ provider "aws" {
   profile = var.aws_profile
 }
 
+module "secrets_manager" {
+  source      = "./modules/secrets_manager"
+  db_password = var.db_password
+  db_username = var.db_username
+}
+
+# Fetch username from Secrets Manager
+data "aws_secretsmanager_secret_version" "db_username" {
+  secret_id = "yourmedia/mysql/username"
+  depends_on = [module.secrets_manager]
+}
+
+# Fetch password from Secrets Manager
+data "aws_secretsmanager_secret_version" "db_password" {
+  secret_id = "yourmedia/mysql/password"
+  depends_on = [module.secrets_manager]
+}
+
 data "aws_rds_engine_version" "mysql" {
   engine = "mysql"
 }
@@ -20,27 +38,27 @@ data "aws_rds_engine_version" "mysql" {
 module "s3" {
   source = "./modules/s3"
 
-  bucket_name = var.bucket_name
-  environment = var.environment
+  bucket_name  = var.bucket_name
+  environment  = var.environment
   sso_role_arn = var.sso_role_arn
 }
 
 module "ec2" {
   source = "./modules/ec2"
 
-  ami_id         = var.ami_id
-  instance_type  = var.instance_type
-  key_name       = var.key_name
-  instance_name  = var.instance_name
-  subnet_id      = "subnet-07294a34e81639bd7" # Reference the public subnet ID from the VPC module
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  instance_name = var.instance_name
+  subnet_id     = "subnet-07294a34e81639bd7" # Reference the public subnet ID from the VPC module
 }
 
 module "vpc" {
   source = "./modules/vpc"
 
-  vpc_cidr          = "10.0.0.0/16"
+  vpc_cidr            = "10.0.0.0/16"
   public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"] # list required
-  vpc_name          = "ecf2025-vpc"
+  vpc_name            = "ecf2025-vpc"
 }
 
 module "rds" {
@@ -50,8 +68,8 @@ module "rds" {
   engine_version       = data.aws_rds_engine_version.mysql.version # dynamically get/fetch the latest version
   instance_class       = "db.t3.micro"
   db_name              = var.db_name
-  username             = var.db_username
-  password             = var.db_password
+  username             = data.aws_secretsmanager_secret_version.db_username.secret_string
+  password             = data.aws_secretsmanager_secret_version.db_password.secret_string
   parameter_group_name = "default.mysql8.0"
   security_group_id    = module.vpc.mysql_security_group_id
   subnet_ids           = module.vpc.public_subnet_ids
